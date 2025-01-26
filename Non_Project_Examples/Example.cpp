1cd0b8cbd299a85281d05ddbafbea6b2fdac39d6
L-A-F-987 <lucafaccenda22@gmail.com>
2025-01-26 23:07:19 +0000

    Setting up a basic CMake file which can be used for various subdirectories. Uploaded a simple example for including a library in c++ that was not related to the actual project. This was done to give those less familar with c++ or rusty a chance to familarise themselves with the basic layout of libraries. These examples will be removed at a later date when the project is fully underway.

---
+++ Non_Project_Examples/Example.cpp
@@ -0,0 +1,10 @@
+
+//including the library I made
+#include "./eexample_header_files/Hello_Team.h"
+
+int main(){
+
+    Print_Out_Names(3);
+
+
+}
---
+++ Non_Project_Examples/eexample_header_files/Hello_Team.h
@@ -0,0 +1,3 @@
+
+//Declaring the function here so that it can be linked to the cpp file
+void Print_Out_Names(int n_prints);
