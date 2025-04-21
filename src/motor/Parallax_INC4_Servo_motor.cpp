2d044ee56ded5a84ec387d7549ca64b9d98b4e55
Tanvi Kapil <tanvikapil2002@gmail.com>
2025-04-21 20:55:05 +0100

    Rectifying servo motor function definitions

--- src/motor/Parallax_INC4_Servo_motor.cpp
+++ src/motor/Parallax_INC4_Servo_motor.cpp
@@ -8,24 +8,34 @@
 
 int Parallax_Motor::start(){
     pwm.start(channel, f); //starting the pwm and supplying frequency and GPIO channel
+
+    return 0;
 }
 
 
 int Parallax_Motor::Set_motor_upwards(){
      pwm.setDutyCycle(6.1);
+
+     return 0;
 };
 
 int Parallax_Motor::Set_motor_stop(){
     pwm.setDutyCycle(7.05);
+
+    return 0;
     
 };
 
 int Parallax_Motor::Set_motor_downwards(){
     pwm.setDutyCycle(8);
+
+    return 0;
     
 };
 
 
 int Parallax_Motor::stop(){
     pwm.stop();
+
+    return 0;
 };
